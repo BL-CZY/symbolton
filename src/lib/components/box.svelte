779e3b5c65eea71ede299243e3db46ecfb5eac9c
BL-CZY <jiguangllsw@gmail.com>
2024-02-27 21:24:18 +0100

    box init

---
+++ src/lib/components/box.svelte
@@ -0,0 +1,28 @@
+<style>
+    div.box {
+        background-color: aliceblue;
+        display: inline-block;
+        width: 10em;
+        height: 10em;
+        border-radius: 10px;
+    }
+
+    div.space {
+        height: 1.5em;
+    }
+
+    label {
+        font-size: 35px;
+    }
+</style>
+
+<script>
+    export let text;
+</script>
+
+<div class="box">
+    <div class="space"></div>
+    <!-- svelte-ignore a11y-label-has-associated-control -->
+    <label class="text">{text}</label><br>
+    <button>copy to clipboard</button>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+    div.main {
+        text-align: center;;
+    }
+    div.container {
+        display: inline-block;
+        padding: 10px;
+    }
+</style>
+
+<script>
+    import Box from "$lib/components/box.svelte";
+    const math = ["Ω", "β", "λ", "δ", "θ", "σ", "τ", "ω", "ɑ", "Σ", "∫"];
+</script>
+
+<div class="main">
+    {#each math as content, index}
+        <div class="container">
+            <Box text={content}/>
+        </div>
+    {/each}
+</div>
