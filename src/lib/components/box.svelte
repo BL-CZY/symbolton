7b60b1dccd2a87849d967d8a380572c06e071c58
BL-CZY <jiguangllsw@gmail.com>
2024-03-03 21:36:04 +0100

    boxes

--- src/lib/components/box.svelte
+++ src/lib/components/box.svelte
@@ -1,28 +1,53 @@
 <style>
     div.box {
-        background-color: aliceblue;
+        background: linear-gradient(to top right, var(--color-1), var(--color-2));
         display: inline-block;
         width: 10em;
         height: 10em;
         border-radius: 10px;
+        transition: all 0.5s cubic-bezier(0.075, 0.82, 0.165, 1);
+        justify-content: center;
     }
 
+    div.inner {
+        background-color: var(--color-3);
+        border-radius: 10px;
+        width: 97%;
+        height: 97%;
+        margin: auto;
+        color: aliceblue;
+    }
+    
+    div.holder {
+        height: 1.5%;
+    }
+    
+    div.box:hover {
+        transform: scale(1.1);
+    }
+    
     div.space {
         height: 1.5em;
     }
-
+    
     label {
         font-size: 35px;
     }
-</style>
+    </style>
 
 <script>
     export let text;
+    export let color1;
+    export let color2;
+    export let color3;
 </script>
 
-<div class="box">
-    <div class="space"></div>
-    <!-- svelte-ignore a11y-label-has-associated-control -->
-    <label class="text">{text}</label><br>
-    <button>copy to clipboard</button>
+<div class="box" style="--color-1: {color1}; --color-2: {color2};">
+    <div class="holder"></div>
+    <div class="inner" style="--color-3: {color3}">
+        <div class="space"></div>
+        <!-- svelte-ignore a11y-label-has-associated-control -->
+        <label class="text">{text}</label><br>
+        <button id="cp_btn">copy to clipboard</button>
+    </div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,45 @@
 <style>
     div.main {
-        text-align: center;;
+        text-align: center;
     }
+    
+    div.sub {
+        margin: auto;
+        width: 80%;
+    }
+
     div.container {
         display: inline-block;
-        padding: 10px;
+        padding: 1em;
+    }
+
+    div.nav {
+        z-index: -1;
     }
 </style>
 
 <script>
     import Box from "$lib/components/box.svelte";
-    const math = ["Ω", "β", "λ", "δ", "θ", "σ", "τ", "ω", "ɑ", "Σ", "∫"];
+    import Nav from "$lib/components/nav.svelte"
+    const math = ["Ω", "β", "λ", "δ", "θ", "σ", "τ", "ω", "ɑ", "Σ", "∫", "≅", "Δ", "π", "≈", "∠", "∟", "°", "⊥", "∥", "≡", "∝", "∞", "⌊⌋", "⌈⌉", "∘", "φ", "⊗", "⊆", "⊂", "⊄", "⊇", "⊃", "⊖", "∈", "∉", "¬", "⇒", "⇔", "↔", "∴", "∵"];
+    const currency = ["€", "β", "λ", "δ", "θ", "σ", "τ", "ω", "ɑ", "Σ", "∫"];
 </script>
 
+<div class="nav">
+</div>
+
 <div class="main">
-    {#each math as content, index}
-        <div class="container">
-            <Box text={content}/>
-        </div>
-    {/each}
+    <div class="sub">
+        {#each math as content, index}
+            <div class="container">
+                <Box text={content} color1="#1d813e" color2="#96ee89" color3="#00123f50"/>
+            </div>
+        {/each}
+        <hr />
+        {#each currency as content, index}
+            <div class="container">
+                <Box text={content} color1="#E0AA3E" color2="#F9F295" color3="#00000050"/>
+            </div>
+        {/each}
+    </div>
 </div>
